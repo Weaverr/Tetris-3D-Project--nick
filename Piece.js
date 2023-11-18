821fc61919f30e2df323092019671b0010f63e8a
Nick Hudson <njhudson25@gmail.com>
2023-11-18 18:43:08 +0000

    Movement and drop works with new system

--- Piece.js
+++ Piece.js
@@ -1,65 +1,103 @@
-class boxPiece {
-    constructor(startbox, startLayer, colour, stroke) {
-        this.layer = startLayer
-        this.box = startbox
-        /* this.box[1] = this.box[0] + 1
-        this.box[2] = this.box[0] + 10
-        this.box[3] = this.box[1] + 10 */
+class boxHandler {
+    constructor(newBox, colour, stroke) {
+        this.boxPos = newBox
         this.colour = colour;
         this.stroke = stroke;
-
-        // Movement distance per step
-        this.distance = 10;
-        this.keyIsPressed = false;
-
-        // Define the grid boundaries (update as per your grid)
-        this.minX = -50;
-        this.maxX = 50; // Example width
-        this.minZ = -50; //fine
-        this.maxZ = 50; // fine
     }
 
-    // Method to handle keyboard inputs
-    handleInputs() {
-        if (!this.keyIsPressed) {
-            if (keyIsDown(87)) { // W key for up
-                this.move('up');
-                this.keyIsPressed = true;
-            } else if (keyIsDown(65)) { // A key for left
-                this.move('left');
-                this.keyIsPressed = true;
-            } else if (keyIsDown(83)) { // S key for down
-                this.move('down');
-                this.keyIsPressed = true;
-            } else if (keyIsDown(68)) { // D key for right
-                this.move('right');
-                this.keyIsPressed = true;
-            }
-        }
-
-        // Reset keyIsPressed if no relevant keys are pressed
-        if (!keyIsDown(87) && !keyIsDown(65) && !keyIsDown(83) && !keyIsDown(68)) {
-            this.keyIsPressed = false;
-        }
-    }
-    // Method to display the piece
     show() {
         push();
         fill(this.colour.r, this.colour.g, this.colour.b);
         stroke(this.stroke.r, this.stroke.g, this.stroke.b);
-        let boxPos = getGridPos(this.box)
-        translate(boxPos.x, getLayerPos(this.layer), boxPos.z);
+        let y = (-20 - (10 * this.boxPos.layerNum))
+        let x = valueCoordinator(this.boxPos.x)
+        let z = valueCoordinator(this.boxPos.z)
+        translate(x, y, z);
+        //better variable names than xyz
         box(10, 10, 10);
         pop();
     }
+
+    drop() {
+        this.boxPos.layerNum = this.boxPos.layerNum - 1
+    }
+
+    getPos() {
+        return (this.boxPos)
+    }
+
+    setPos(newBox) {
+        this.boxPos = newBox
+    }
 }
 
-function getLayerPos(layerNum) {
-    return (-20 - (10 * layerNum))
+
+function valueCoordinator(value) {
+    //make better name lol
+    //error with single equals
+    if (value == 0) { return (-45) }
+    if (value == 1) { return (-35) }
+    if (value == 2) { return (-25) }
+    if (value == 3) { return (-15) }
+    if (value == 4) { return (-5) }
+    if (value == 5) { return (5) }
+    if (value == 6) { return (15) }
+    if (value == 7) { return (25) }
+    if (value == 8) { return (35) }
+    if (value == 9) { return (45) }
 }
 
-function getGridPos(gridNum) {
-    if (gridNum == 1){ return { x: -45, z: -45 }}
-    if (gridNum == 2){ return { x: -45, z: -35 } }
-    
+
+class peice {
+    constructor(peiceType, masterLocation, color, fill) {
+        this.boxArr = []
+        this.masterLocation = masterLocation
+        this.peiceType = peiceType
+        this.color = color;
+        this.fill = fill;
+        this.updatePos()
+    }
+
+    show() {
+        for (let i = 0; i < this.boxArr.length; i++) {
+            this.boxArr[i].show()
+        }
+    }
+
+    drop() {
+        for (let i = 0; i < this.boxArr.length; i++) {
+            this.boxArr[i].drop()
+        }
+    }
+
+    getMasterPos() {
+        return (this.boxArr[0].getPos())
+    }
+
+    setPos(masterLocation) {
+        this.masterLocation = masterLocation
+        this.updatePos()
+    }
+
+    updatePos() {
+        if (this.peiceType == 0) {
+            //2x2box
+            //peicetype left as number so it can be randomised
+            this.boxArr[0] = new boxHandler({ x: this.masterLocation.x, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[1] = new boxHandler({ x: this.masterLocation.x + 1, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[2] = new boxHandler({ x: this.masterLocation.x, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z + 1 }, this.color, this.fill);
+            this.boxArr[3] = new boxHandler({ x: this.masterLocation.x + 1, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z + 1 }, this.color, this.fill);
+            this.boxArr[4] = new boxHandler({ x: this.masterLocation.x, layerNum: this.masterLocation.layerNum + 1, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[5] = new boxHandler({ x: this.masterLocation.x + 1, layerNum: this.masterLocation.layerNum + 1, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[6] = new boxHandler({ x: this.masterLocation.x, layerNum: this.masterLocation.layerNum + 1, z: this.masterLocation.z + 1 }, this.color, this.fill);
+            this.boxArr[7] = new boxHandler({ x: this.masterLocation.x + 1, layerNum: this.masterLocation.layerNum + 1, z: this.masterLocation.z + 1 }, this.color, this.fill);
+        }
+
+    }
+
+    getDimensions() {
+        if (this.peiceType == 0) {
+            return ({ x: 2, y: 2, z: 2 })
+        }
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,47 +1,28 @@
 // Variables
-let myPiece;
-let myLPiece;
-let currentcamera;
 let myTetrisgridL;
 let myTetrisgridR;
 let myTetrisgridF;
-let keyIsPressed = false;
 let timer = 500;
-
-// Define boundaries for Tetris grid walls
-const minX = -50;
-const maxX = 50;
-const minZ = -50;
-const maxZ = 50;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   rectMode(CENTER);
   angleMode(DEGREES);
   currentcamera = new devCam(-250, -300, 250, 0, -100, 0);
-
   setInterval(() => Gravity(), timer);
+  myTetrisgridL = new Tetrisgrid(100, 300, 0, 50, -155, 1, 0, 90, 255, { r: 0, g: 0, b: 0 });
+  myTetrisgridR = new Tetrisgrid(100, 300, 0, 0, -155, -50, 0, 0, 255, { r: 0, g: 0, b: 0 });
+  myTetrisgridF = new Tetrisgrid(100, 100, 0, 0, -5, 0, 90, 0, 255, { r: 0, g: 0, b: 0 });
 
-  myTetrisgridL = new Tetrisgrid(100, 300, 0, 50, -150, 1, 0, 90, 255, { r: 0, g: 0, b: 0 });
-  myTetrisgridR = new Tetrisgrid(100, 300, 0, 0, -150, -50, 0, 0, 255, { r: 0, g: 0, b: 0 });
-  myTetrisgridF = new Tetrisgrid(100, 100, 0, 0, 0, 0, 90, 0, 255, { r: 0, g: 0, b: 0 });
+  box1 = new peice(0, { x: 0, layerNum: 6, z: 0, }, { r: 0, g: 0, b: 255 }, { r: 255, g: 255, b: 255 })
 
-
-
-
-  myPiece = new boxPiece(2, 5, { r: 0, g: 0, b: 255 }, { r: 255, g: 255, b: 255 });
-  //myLPiece = new LPiece(0, -100, 0, {r: 255, g: 0, b: 0}, 0, 0);
 }
 let Gravity = () => {
-  myPiece.layer -= 1
-  if (myPiece.layer < 1) {
-    myPiece.layer = 0
+  if (!(box1.getMasterPos().layerNum < 0)) {
+    box1.drop()
   }
 }
 
-function cam() {
-  return currentcamera;
-}
 
 function draw() {
   inputs();
@@ -55,8 +36,7 @@ function inputs() {
 
 
 function processes() {
-  //myLPiece.move();
-  myPiece.handleInputs();
+
 }
 
 
@@ -64,26 +44,51 @@ function outputs() {
   background(255, 255, 255);
   currentcamera.show();
   stroke(255);
-  myPiece.show();
-  //myLPiece.show();
+  box1.show()
   stroke('lime')
   myTetrisgridL.show();
   myTetrisgridR.show();
   myTetrisgridF.show();
 }
 
-function moveUp() {
-  myPiece.z -= distance;
-}
 
-function moveDown() {
-  myPiece.z += distance; // Adjust the value as needed
-}
 
-function moveLeft() {
-  myPiece.x -= distance; // Adjust the value as needed
-}
+function keyPressed() {
+  // replace box1 with activeBox
+  if (keyCode == '87') {
+    //w
+    let oldPos = box1.getMasterPos()
+    if (!(oldPos.x >= (9 - (box1.getDimensions().x - 1)))) {
+      //VALIDATION, REMOVE IF AND ADD INTO WRITEUP FOR MARKS
+      box1.setPos({ x: oldPos.x + 1, layerNum: oldPos.layerNum, z: oldPos.z })
+    }
+  }
 
-function moveRight() {
-  myPiece.x += distance; // Adjust the value as needed
+  if (keyCode == '83') {
+    //s
+    let oldPos = box1.getMasterPos()
+    if (!(oldPos.x <= 0)) {
+      //VALIDATION, REMOVE IF AND ADD INTO WRITEUP FOR MARKS
+      box1.setPos({ x: oldPos.x - 1, layerNum: oldPos.layerNum, z: oldPos.z })
+    }
+  }
+
+  if (keyCode == '65') {
+    //a
+    let oldPos = box1.getMasterPos()
+    if (!(oldPos.z <= 0)){
+      //VALIDATION, REMOVE IF AND ADD INTO WRITEUP FOR MARKS
+      box1.setPos({ x: oldPos.x, layerNum: oldPos.layerNum, z: oldPos.z - 1 })
+    }
+  }
+
+  if (keyCode == '68') {
+    //d
+    let oldPos = box1.getMasterPos()
+    if (!(oldPos.z >= (9 - (box1.getDimensions().z - 1)))) {
+      //VALIDATION, REMOVE IF AND ADD INTO WRITEUP FOR MARKS
+      box1.setPos({ x: oldPos.x, layerNum: oldPos.layerNum, z: oldPos.z + 1 })
+    }
+  }
+  //console.log(event)
 }
